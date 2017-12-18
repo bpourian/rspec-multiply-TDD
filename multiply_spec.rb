07ef12120f77b49096bce5b685142ab69d386d69
Benjamin Pourian <b.pourian@hotmail.co.uk>
2017-12-18 22:57:38 +0000

    first commit rspec file

---
+++ multiply_spec.rb
@@ -0,0 +1,19 @@
+require "./multiply_number.rb"
+
+describe 'multiplier' do
+  it "multiplies positive integer numbers by 2" do
+    expect(multiply(10)).to eq 20
+  end
+end
+
+describe 'multiplier' do
+  it "multiplies positive integer numbers by 2" do
+    expect(multiply(30)).to eq 60
+  end
+end
+
+describe 'multiplier' do
+  it "multiplies positive integer numbers by 2" do
+    expect(multiply(121)).to eq 242
+  end
+end
