1f7a6c61449731b416f7a8c6920bbbdd68b64e0a
Benjamin Pourian <b.pourian@hotmail.co.uk>
2017-12-18 23:07:07 +0000

    refactored rspec code

--- multiply_spec.rb
+++ multiply_spec.rb
@@ -4,15 +4,9 @@ describe 'multiplier' do
   it "multiplies positive integer numbers by 2" do
     expect(multiply(10)).to eq 20
   end
-end
-
-describe 'multiplier' do
   it "multiplies positive integer numbers by 2" do
     expect(multiply(30)).to eq 60
   end
-end
-
-describe 'multiplier' do
   it "multiplies positive integer numbers by 2" do
     expect(multiply(121)).to eq 242
   end
